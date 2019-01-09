0815f1125980e8aef27b05899fbcc7f4547c7f82
ImadMebrouk <mebrouk.imad@gmail.com>
2019-01-09 23:48:07 +0100

    AddFriend() and DeleteFriend()
    Add Friend is working 
    DeleteFriend = To Do

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import redis
 
+
 r = redis.Redis(
     host = 'localhost',
     port=6379)
 
-
-#r.set('foo', 'bar')
-#value = r.get('foo')
-#print(value)
 
 def create_user(id_attr, **kwargs):
     user_id =("user." + kwargs['id'])
@@ -24,12 +21,22 @@ def get_user_by_id(user_id):
     keys = {"username", "first_name","last_name"}
     return r.hmget('user.' + user_id, keys)
 
+def add_Friend(user_id, friend_id):
+    
+    r.lpush('user.'+user_id+'.friendsList', friend_id)
+    r.lpush('user.'+friend_id+'.friendsList', user_id)
+
+def  delete_friend(user_id, friend_id): #toDo 
+    return null
+
 Imad = {
     'id': '1',
     'username': 'coach',
     'password': 'password',
     'first_name': 'Imad',
     'last_name': 'Mebrouk',
+    'age':'23',
+    'gender':'male',
     'location': 'Sannois'
 }
 
@@ -37,3 +44,4 @@ Imad = {
 create_user('coach', **Imad)
 
 print(get_user_by_id('1'))
+
