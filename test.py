3ca03e507185f96b998a4dc6771ce687a60e0084
ImadMebrouk <mebrouk.imad@gmail.com>
2019-01-12 13:04:56 +0100

    Update test.py

--- test.py
+++ test.py
@@ -26,12 +26,31 @@ def create_user( **kwargs):
         
         
     r.incr("compteur")
-    return r.hget(usr, "user_id")
- 
+    return r.hget(user, "user_id")
 
+def user_connection(username, pwd):
+    connection_success = False
+    if(get_user_by_username(username)):
+        current_password = str(r.hget("user:"+str(get_user_by_username(username)), "password"))
+        if(current_password == pwd):
+            connection_success = True
+
+
+
+    return connection_success
+
+def get_user_by_username(username):
+    user_id =1
+    user = "user:" + str(user_id)
+    while r.hget(user,"username") != None:
+        current_user =(str(r.hget(user,"username")))
+        if current_user == username:
+            return user_id  #user id of the corresponding username 
+        else:
+            user_id+=1
+    return false # no user found with this username 
 
 def get_user_by_id(user_id):
-
 
     username = r.hget("user:" + str(user_id), "username")
 
@@ -51,19 +70,21 @@ def  delete_friend(user_id, friend_id): #toDo
     return null
 
 Imad = {
-    'username': 'coach',
-    'password': 'password',
-    'first_name': 'Imad',
-    'last_name': 'Mebrouk',
-    'age':'23',
-    'gender':'male',
-    'location': 'Sannois',
-    'relation': "",
-    'posts': ""
+    "username": "coach",
+    "password": "password",
+    "first_name": "Imad",
+    "last_name": "Mebrouk",
+    "age":"23",
+    "gender":"male",
+    "location": "Sannois",
+    "friendsList": "",
+    "posts": ""
     }
 
 
 create_user(**Imad)
 
 print(get_user_by_id(1))
+
+print(user_connection("b'coach'", "b'password'")) # don't understand why "b" is added when i do hget(something)
 
